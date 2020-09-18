33c0ec0a4f86712a7fbd24d29b327449eeebc9bb
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2020-09-18 14:58:54 -0700

    Made JQuery plan of action

--- 02-Homework/script.js
+++ 02-Homework/script.js
@@ -5,5 +5,34 @@ Rewrite most of html by using JQuery:
 Map out functions needed: 
 Set up local storage:
 */
+let myWeatherKey = 'f67fdb4a68c4728ac9398f3aa4b198ce';
+
+$(document).ready(function(){
+
+// Local storeage get and store
+
+// On click for save button and clear button
+
+/* AJAX call for different cities the tracks and displays the following:
+                            current weather(date: mm/dd/yyyy): sunny or other, temp, humidity, uv index, and wind speeds
+                            future(5-day)forecast: sunny or other, temp, humidity*/
+// Find out if the above can be put into a display function
+
+// Loop to add a new row to the far left side of cities researched
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+})
 
 
