72eba67d99845fc7b7ba7193d5106295d75ea848
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2020-09-18 14:29:52 -0700

    Started Skeleton

---
+++ 02-Homework/script.js
@@ -0,0 +1,9 @@
+/* To Do list:
+Start a basic html with some css: Started
+Research more effic. ways...:
+Rewrite most of html by using JQuery:
+Map out functions needed: 
+Set up local storage:
+*/
+
+
