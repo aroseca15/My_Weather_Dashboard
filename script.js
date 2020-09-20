b007c0a726d4b8fe858d8a046eab2f0603071047
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2020-09-20 14:19:15 -0700

    Tried some different types on syntax. Still missing something.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ Set up local storage:
 */
 
 
-{/* <section id="forecasts">
+/* <section id="forecasts">
             <div class="row">
                 <div id="current-forecast" class="col-md-6">
                     <h1>Five Day Forecast</h1>
@@ -15,7 +15,7 @@ Set up local storage:
                 <div class="col-md-6">
                     <h1>Current Forecast</h1>
                 </div>
-            </div> */}
+            // </div> */
 
 
 
@@ -28,11 +28,9 @@ let myWeatherKey = 'd162cce18ba9a5542669d67133ba8953';
 
 $(document).ready(function () {
 
-    let newRow = $('<div>').addClass('row').addId('fiveDayForecast');
-    let fiveDayForecast = $('<div>').addClass('col-6').text('Your 5 Day Forecast');
-    // console.log(fiveDayForecast);
-    // fiveDayForecast.text('Your 5 Day Forecast');
-    newRow.append(fiveDayForecast);
+    let fiveDayForecast = $('<div>').addClass('col-md-6').addId('fiveDayForecast');
+    fiveDayForecast.text("Your 5 Day Forecast")
+    fiveDayForecast.innerHTML().append('forecasts');
     container-fluid.append(newRow);
 
 
@@ -56,39 +54,39 @@ $(document).ready(function () {
     // })
 
     // On click for save button and clear button
-    $('#save-btn').on('click', function () {
-        let inputVal = $("#input").val();
-        currentWeather(inputVal);
+    // $('#save-btn').on('click', function () {
+    //     let inputVal = $("#input").val();
+    //     currentWeather(inputVal);
         // let time = $(this).attr('date-time');
         // localStorage.setItem(time, value);
-    });
+    // });
 
-    $('#clear-btn').on('click', function () {
+    // $('#clear-btn').on('click', function () {
 
-    });
+    // });
 
 
-    function currentWeather(cityName) {
-        let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${myWeatherKey}`
+    // function currentWeather(cityName) {
+    //     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${myWeatherKey}`
             
-        let currentDate = new Date().toLocaleString();
+    //     let currentDate = new Date().toLocaleString();
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
+    //     $.ajax({
+    //         url: queryURL,
+    //         method: "GET"
+    //     }).then(function (response) {
 
-            console.log(response);
-            // city
-            // temp
-            // wind
-            // humitiy
-            // UV Index
+    //         console.log(response);
+    //         // city
+    //         // temp
+    //         // wind
+    //         // humitiy
+    //         // UV Index
             
-        })
-        }
+    //     })
+    //     }
 
-    currentWeather(cityName);
+    // currentWeather(cityName);
     /* AJAX call for different cities the tracks and displays the following:
     
     
