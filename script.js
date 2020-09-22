01ef2d54f1aac1505ed69a026d1f17daece2db8e
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2020-09-21 17:50:30 -0700

    Tried changing and removing variables according to udemy class. Also, formatted.

--- script.js
+++ script.js
@@ -15,52 +15,52 @@ Set up local storage:
                 </div>
                 <div class="col-md-6">
                     <h1>Current Forecast</h1>
-                </div>
-            // </div> */
+//                 </div>
+//             // </div> */
 
-let day1= $('<div>').attr('id','day1').addClass('row');
-let day2= $('<div>').attr('id','day2').addClass('row');
-let day3= $('<div>').attr('id','day3').addClass('row');
-let day4= $('<div>').attr('id','day4').addClass('row');
-let day5= $('<div>').attr('id','day5').addClass('row');
-
-
+// let day1= $('<div>').attr('id','day1').addClass('row');
+// let day2= $('<div>').attr('id','day2').addClass('row');
+// let day3= $('<div>').attr('id','day3').addClass('row');
+// let day4= $('<div>').attr('id','day4').addClass('row');
+// let day5= $('<div>').attr('id','day5').addClass('row');
 
 
-let myWeatherKey = 'd162cce18ba9a5542669d67133ba8953';
-// let CurrQueryURL1= 'api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={your api key}';
-// let FiveDayQueryURL= 'api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid={your api key}';
+
+
+// let myWeatherKey = 'd162cce18ba9a5542669d67133ba8953';
+// // let CurrQueryURL1= 'api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={your api key}';
+// // let FiveDayQueryURL= 'api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid={your api key}';
 
 $(document).ready(function () {
 
-    let fiveDayForecast = $('<div>').addClass('col-md-6');
-    fiveDayForecast.append($('<h1>').text('5 Day Forecast'));
-    fiveDayForecast.append($('<div>').attr('id', 'day1').addClass('row'));
-    // fiveDayForecast.append($('<h5>').text('Temp:'));
-    // fiveDayForecast.append($('<h5>').text('humidity'));
-    fiveDayForecast.append($('<div>').attr('id', 'day2').addClass('row'));
-    // fiveDayForecast.append($('<h5>').text('Temp:'));
-    // fiveDayForecast.append($('<h5>').text('humidity'));
-    fiveDayForecast.append($('<div>').attr('id', 'day3').addClass('row'));
-    // fiveDayForecast.append($('<h5>').text('Temp:'));
-    // fiveDayForecast.append($('<h5>').text('humidity'));
-    fiveDayForecast.append($('<div>').attr('id', 'day4').addClass('row'));
-    // fiveDayForecast.append($('<h5>').text('Temp:'));
-    // fiveDayForecast.append($('<h5>').text('humidity'));
-    fiveDayForecast.append($('<div>').attr('id', 'day5').addClass('row'));
-    // fiveDayForecast.append($('<h5>').text('Temp:'));
-    // fiveDayForecast.append($('<h5>').text('humidity'));
+    //     let fiveDayForecast = $('<div>').addClass('col-md-6');
+    //     fiveDayForecast.append($('<h1>').text('5 Day Forecast'));
+    //     fiveDayForecast.append($('<div>').attr('id', 'day1').addClass('row'));
+    //     // fiveDayForecast.append($('<h5>').text('Temp:'));
+    //     // fiveDayForecast.append($('<h5>').text('humidity'));
+    //     fiveDayForecast.append($('<div>').attr('id', 'day2').addClass('row'));
+    //     // fiveDayForecast.append($('<h5>').text('Temp:'));
+    //     // fiveDayForecast.append($('<h5>').text('humidity'));
+    //     fiveDayForecast.append($('<div>').attr('id', 'day3').addClass('row'));
+    //     // fiveDayForecast.append($('<h5>').text('Temp:'));
+    //     // fiveDayForecast.append($('<h5>').text('humidity'));
+    //     fiveDayForecast.append($('<div>').attr('id', 'day4').addClass('row'));
+    //     // fiveDayForecast.append($('<h5>').text('Temp:'));
+    //     // fiveDayForecast.append($('<h5>').text('humidity'));
+    //     fiveDayForecast.append($('<div>').attr('id', 'day5').addClass('row'));
+    //     // fiveDayForecast.append($('<h5>').text('Temp:'));
+    //     // fiveDayForecast.append($('<h5>').text('humidity'));
 
 
 
-    $('#forecasts').append(fiveDayForecast);
+    //     $('#forecasts').append(fiveDayForecast);
 
-    let currentForecast = $('<div>').addClass('col-md-6');
-    currentForecast.append($('<h1>').text('Current Forecast'));
-    $('#forecasts').append(currentForecast);
-    
+    //     let currentForecast = $('<div>').addClass('col-md-6');
+    //     currentForecast.append($('<h1>').text('Current Forecast'));
+    //     $('#forecasts').append(currentForecast);
 
-    
+
+
 
 
 
@@ -73,76 +73,74 @@ $(document).ready(function () {
         let savedInput = localStorage.getItem(retrieve);
         $(this).val(savedInput);
 
-        })
+    });
 
 
-        // Local Storage save function
-        
+    // Local Storage save function
 
-        // On click for save button and clear button
-        $('#save-btn').on('click', function () {
+
+    // On click for save button and clear button
+    $('#save-btn').on('click', function () {
+        let inputVal = $("#input").val();
+        currentWeather(inputVal);
+        // let time = $(this).attr('date-time');
+        JSON.stringify(inputVal);
+        localStorage.setItem(value);
+
+        $('#input').change(function () {
             let inputVal = $("#input").val();
             currentWeather(inputVal);
-        let time = $(this).attr('date-time');
-        JSON.stringify(inputVal);
-        localStorage.setItem(time, value);
-
-        $('#input').change(function(){
-            let inputVal = $("#input").val();
-            currentWeather(inputVal);
-        let time = $(this).attr('date-time');
-        JSON.stringify(inputVal);
-        localStorage.setItem(time, value);
+            // let time = $(this).attr('date-time');
+            JSON.stringify(inputVal);
+            localStorage.setItem(value);
         })
-        });
-
-        
-        // $('#clear-btn').on('click', function () {
-
-        // });
-
-        
-        function currentWeather(cityName) {
-            let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${myWeatherKey}`
-            
-            // let currentDate = new Date().toLocaleString();
-            // let cityName= 'city name'
-            // let cityName= inputVal.stringify()
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function (response) {
-
-                console.log(response);
-                // city
-                // temp
-                // wind
-                // humitiy
-                // UV Index
-
-            })
-            }
-            
-        currentWeather(cityName);
+    });
 
 
-        function getFiveDayFore(inputVal){
-            let queryURL=`https://api.openweathermap.org/data/2.5/forecast?q=${inputVal}&appid=${myWeatherKey}`
-            $.ajax({
-
-                URL: queryURL,
-                method: 'GET' 
-
-            }).then(function(response){
-                console.log(response)
-            })
-
-        }
-
-        getFiveDayFore();
+    $('#clear-btn').on('click', function () {
+        let inputVal = $("#input").val();
+        localStorage.clear(inputVal);
+    });
 
 
+    // function currentWeather(cityName) {
+    //     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${myWeatherKey}`
 
+    //     // let currentDate = new Date().toLocaleString();
+    //     // let cityName= 'city name'
+    //     // let cityName= inputVal.stringify()
+    //     $.ajax({
+    //         url: queryURL,
+    //         method: "GET"
+    //     }).then(function (response) {
+
+    //         console.log(response);
+    //         // city
+    //         // temp
+    //         // wind
+    //         // humitiy
+    //         // UV Index
+
+    //     })
+    //     }
+
+    // currentWeather(cityName);
+
+
+    // function getFiveDayFore(inputVal){
+    //     let queryURL=`https://api.openweathermap.org/data/2.5/forecast?q=${inputVal}&appid=${myWeatherKey}`
+    //     $.ajax({
+
+    //         URL: queryURL,
+    //         method: 'GET' 
+
+    //     }).then(function(response){
+    //         console.log(response)
+    //     })
+
+    // }
+
+    // getFiveDayFore();
 
 
 
@@ -150,6 +148,9 @@ $(document).ready(function () {
 
 
 
-    })
+
+
+
+})
 
 
