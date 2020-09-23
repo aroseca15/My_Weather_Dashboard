59dd8e1c87db958a9a05903dfa049fad33e3faa5
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2020-09-23 14:44:41 -0700

    Tried working with ajax

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
+let storedCity= '';
+// current weather:
+let currentCity= $('#current-city'); 
+let currentDateDis= $('#current-date');
+let currentTemp= $('#current-temp');
+let currentWind= $('#current-windSpeeds');
+let currentUv= $('#current-uv');
+
+// 5 day forecast:
 
 
 
 
-
-
-
-
-// let myWeatherKey = 'd162cce18ba9a5542669d67133ba8953';
-// // let CurrQueryURL1= 'api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={your api key}';
-// // let FiveDayQueryURL= 'api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid={your api key}';
 
 $(document).ready(function () {
 
-let text1= 'Conditions: '
-let text2= 'Temp: '
-let text3= 'Humidity: '
-$('.card-title').append(text1);
-$('.card-subtitle').append(text2);
-$('.card-text').append(text3);
+// let text1= 'Conditions: '
+// let text2= 'Temp: '
+// let text3= 'Humidity: '
+// $('.card-title').append(text1);
+// $('.card-subtitle').append(text2);
+// $('.card-text').append(text3);
 
-
-        let currentForecast = $('<div>').addClass('col-md-6');
-        currentForecast.append($('<h1>').text('Current Forecast'));
-        $('#forecasts').append(currentForecast);
 
 
 
@@ -42,33 +40,50 @@ $('.card-text').append(text3);
 
 
     // Local Storage save function
-
+    let historyArray= [];
 
     // On click for save button and clear button
-    $('#save-btn').on('click', function () {
+    $('#save-btn').on('click', function (event) {
         event.preventDefault()
         let inputVal = $("#input").val();
+        
+        historyArray[0] = inputVal;
+        localStorage.setItem("city", JSON.stringify(inputVal));
         currentWeather(inputVal);
-        // let time = $(this).attr('date-time');
-        JSON.stringify(inputVal);
-        localStorage.setItem('city', inputVal);
-        console.log(inputVal);
+        let time = $(this).attr('date-time');
 
-        $('#input').change(function () {
-            let inputVal = $("#input").val();
-            event.preventDefault()
-            currentWeather(inputVal);
-            let time = $(this).attr('date-time');
-            JSON.stringify(inputVal);
-            localStorage.setItem(value);
-        })
+        let currentS= localStorage.getItem('city');
+        currentS = JSON.parse(currentS);
+        
+        historyArray.push(inputVal);
+        $('#history').append(historyArray);
+        
+        // localStorage.setItem('city', JSON.stringify(historyArray));
+        getFiveDayFore(inputVal);
+        
+        console.log(historyArray)
+
+        
+        
+        
+        
+
+
+        // $('#input').change(function (event) {
+        //     let inputVal = $("#input").val();
+        //     event.preventDefault()
+        //     currentWeather(inputVal);
+        //     let time = $(this).attr('date-time');
+        //     JSON.stringify(inputVal);
+        //     localStorage.setItem(value);
+        // })
     });
 
-
+    let myWeatherKey = 'd162cce18ba9a5542669d67133ba8953';
     function currentWeather(cityName) {
         let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${myWeatherKey}`
 
-        // let currentDate = new Date().toLocaleString();
+        let currentDate = new Date().toLocaleString();
         // let cityName= 'city name'
         // let cityName= inputVal.stringify()
         $.ajax({
@@ -77,6 +92,7 @@ $('.card-text').append(text3);
         }).then(function (response) {
 
             console.log(response);
+            currentDate.html(currentDateDis);
             // city
             // temp
             // wind
@@ -86,14 +102,12 @@ $('.card-text').append(text3);
         })
         }
 
-    currentWeather(cityName);
-
 
     function getFiveDayFore(inputVal){
         let queryURL=`https://api.openweathermap.org/data/2.5/forecast?q=${inputVal}&appid=${myWeatherKey}`
         $.ajax({
 
-            URL: queryURL,
+            url: queryURL,
             method: 'GET' 
 
         }).then(function(response){
@@ -101,9 +115,6 @@ $('.card-text').append(text3);
         })
 
     }
-
-    getFiveDayFore();
-
 
 
 
